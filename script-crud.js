738b23a48b9c7ff96df6198a23df80296f99ef60
Luis Henrique <Henrique_rzs@hotmail.com>
2024-01-04 06:53:39 -0300

    Terceira aula

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
@@ -25,6 +29,15 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
+
+    botao.onclick = () => {
+        const novaDrecricao = prompt('Qual é o novo nome da tarefa?')
+        if (novaDrecricao) {
+            paragrafo.textContent = novaDrecricao
+            tarefa.descricao = novaDrecricao
+            atualizarTarefas()
+        }
+    }
 
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', './imagens/edit.png')
@@ -49,7 +62,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas()
     textArea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
